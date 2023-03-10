97369eff7036e16f14eb6e762e28845ee448f452
l3miage-delagral <alex.del57640@gmail.com>
2023-03-10 09:31:09 +0100

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'projet-miahoot-cd381',
+    appId: '1:60945598274:web:bf1437eddf117689e42adb',
+    storageBucket: 'projet-miahoot-cd381.appspot.com',
+    apiKey: 'AIzaSyAjZauOKLKMnrlH-OGmUY5XAcdhFYbh5ok',
+    authDomain: 'projet-miahoot-cd381.firebaseapp.com',
+    messagingSenderId: '60945598274',
+  },};

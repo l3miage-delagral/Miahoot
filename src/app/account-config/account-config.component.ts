68f28fe391541237432324adaa386996c52c9657
l3miage-delagral <alex.del57640@gmail.com>
2023-03-17 11:54:07 +0100

    essaye dans account config

--- src/app/account-config/account-config.component.ts
+++ src/app/account-config/account-config.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, OnDestroy, Optional } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { updateDoc } from '@firebase/firestore';
+import { DataService, MiahootUser } from '../data.service';
 
 
 @Component({
@@ -8,4 +12,22 @@ import { Component, OnInit, OnDestroy, Optional } from '@angular/core';
 })
 export class AccountConfigComponent {
   
+  private user!: MiahootUser;
+  private newName!: string;
+
+  constructor(private userDataService: DataService){
+    this.fireStore.then(user => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  }
+
+  updateName() {
+    if (this.user && this.newName) {
+      const userRef = this.userDataService.uid;
+      updateDoc(userRef, { name: this.newName });
+    }
+  }
+
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
-import { DocumentData, FirestoreDataConverter, QueryDocumentSnapshot } from '@angular/fire/firestore';
-import { doc } from '@firebase/firestore';
+import { EventEmitter, Inject, Injectable } from '@angular/core';
+import { Auth, authState, User, user } from '@angular/fire/auth';
+import { docData, DocumentData, FirestoreDataConverter, getDoc, QueryDocumentSnapshot, setDoc } from '@angular/fire/firestore';
+import { doc, Firestore } from '@firebase/firestore';
+import { filter, map, Observable, of, switchMap, tap } from 'rxjs';
 
 
 
-interface MiahootUser {
+export interface MiahootUser {
   readonly name: String;
   readonly photoURL: String;
 }
@@ -27,7 +29,41 @@ const postConverter: FirestoreDataConverter<MiahootUser> = {
 
 export class DataService {
 
-  constructor() { }
+  readonly name !: String;
+  readonly photoURL !: String;
+  private obsMia = new Observable<MiahootUser | undefined>();
+  private obsUser = authState(this.auth);
+
+  constructor(private auth: Auth, private fireStore: Firestore) {
+      authState(this.auth).pipe(
+      filter( u => !!u ),
+      map( u => u as User ),
+      tap( async u => {
+        const docUser =  doc(this.fireStore , `users/${u.uid}`).withConverter(postConverter) ;
+        const snapUser = await getDoc( docUser );
+        if (!snapUser.exists()) {
+          setDoc(docUser, {
+            name: u.displayName ?? u.email ?? u.uid,
+            photoURL: u.photoURL ?? "https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          } satisfies MiahootUser)
+        }
+      })
+    ).subscribe()
+    
+    
+    this.obsMia = this.obsUser.pipe(
+      switchMap((user) => {
+        if(user){
+          const userRef = doc(this.fireStore , `users/${user.uid}`).withConverter(postConverter)
+          const userData$ = docData(userRef)
+          
+          return userData$;
+        } else {
+        // Si l'utilisateur est null, retourner l'observable qui publie undefined
+        return of(undefined);
+        }
+      })
+    );
+  }
+
 }
-
-
